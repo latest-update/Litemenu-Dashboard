6d6d9adcd06f37c80416a947d34eea9b7b23d1e2
Rakhat <95361982+latest-update@users.noreply.github.com>
2023-05-01 19:30:36 +0600

    Change Head to new experimental branch

--- app/Http/Livewire/Dashboard/Pages/Company/Home.php
+++ app/Http/Livewire/Dashboard/Pages/Company/Home.php
@@ -3,14 +3,31 @@
 namespace App\Http\Livewire\Dashboard\Pages\Company;
 
 use App\Models\Company;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Livewire\Component;
+use Livewire\ImplicitRouteBinding;
+use Illuminate\Routing\Route;
 
 class Home extends Component
 {
-    public Company $company;
-    public function mount(Company $company)
+
+    public function mount()
     {
-        $this->company = $company;
+//        $router = app(Route::class);
+//        try {
+//            $componentParams = (new ImplicitRouteBinding(app()))
+//                ->resolveAllParameters($router, $this);
+//        } catch (ModelNotFoundException $exception) {
+//            if (method_exists($router,'getMissing') && $router->getMissing()) {
+//                return $router->getMissing()(request());
+//            }
+//
+//            throw $exception;
+//        }
+//
+
+        
+
     }
 
     public function render()
--- resources/views/livewire/dashboard/pages/company/home.blade.php
+++ resources/views/livewire/dashboard/pages/company/home.blade.php
@@ -9,7 +9,7 @@
                     </div>
                     <div class="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
                         <p class="text-sm font-medium text-gray-600">Welcome to,</p>
-                        <p class="text-xl font-bold text-gray-900 sm:text-2xl">{{ $company->name }}</p>
+                        <p class="text-xl font-bold text-gray-900 sm:text-2xl">McDonalds</p>
                         <p class="text-sm font-medium text-gray-600">Los Angeles, Miles st. 6</p>
                     </div>
                 </div>
