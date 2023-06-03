390e8dded3f62c01503cd281d2830cd5c45aa703
Rakhat <107846206+bcorpj@users.noreply.github.com>
2023-06-03 16:00:15 +0600

    Save changes before pull
    Probably changed async

--- resources/views/livewire/dashboard/pages/sandbox/tables.blade.php
+++ resources/views/livewire/dashboard/pages/sandbox/tables.blade.php
@@ -1,6 +1,6 @@
 <div>
     <x-common.table.power-grid name="Tables of {{ $branch->name }}">
-        <x-button positive label="Create table" class="mt-2" wire:click="new"/>
+        <x-button positive label="Create table" class="my-2" wire:click="new"/>
         <livewire:branch-tables />
     </x-common.table.power-grid>
     <livewire:components.modals.tables.qr />
